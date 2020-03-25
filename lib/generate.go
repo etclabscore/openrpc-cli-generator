d987379986f99e1c79967e5acd66282a685807f1
meows <b5c6@protonmail.com>
2020-03-25 07:04:28 -0500

    lib,lib/templates/cli.gotmpl,lib/templates/cli_cmd.gotmpl,lib/templates/example-proxy-server.gotmpl,lib/templates/server.gotmpl,lib/templates/types.gotmpl: add templates and logic from go-openrpc cli generator
    Signed-off-by: meows <b5c6@protonmail.com>

---
+++ lib/generate.go
@@ -0,0 +1,24 @@
+package lib
+
+import (
+	"github.com/gobuffalo/packr/v2"
+	"github.com/gregdhill/go-openrpc/generate"
+	"github.com/gregdhill/go-openrpc/parse"
+	"github.com/gregdhill/go-openrpc/types"
+)
+
+func GenerateCLI(spec1 *types.OpenRPCSpec1, programName string) error {
+	parse.GetTypes(spec1, spec1.Objects)
+	box := packr.New("template", "./templates")
+
+	generate.ProgramName = programName
+	err := generate.WriteFile(box, "cli", "main", spec1)
+	if err != nil {
+		return err
+	}
+	err = generate.WriteFile(box, "cli_cmd", "cmd", spec1)
+	if err != nil {
+		return err
+	}
+	return nil
+}
